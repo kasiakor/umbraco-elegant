00e7a16f05f2f4b3ef67f8ddb3ecdf530bda3353
Kasia Kordys <kasia.kordys@gmail.com>
2020-03-09 19:38:49 +0000

    add error and xml sitemap view

--- CleanBlog.Web/App_Data/Models/models.generated.cs
+++ CleanBlog.Web/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "5197d79239779f06")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 namespace Umbraco.Web.PublishedModels
 {
