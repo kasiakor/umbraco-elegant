46b4e5fd05006a3796b624800e7c113f12a7e0dd
Kasia Kordys <kasia.kordys@gmail.com>
2020-03-19 22:45:46 +0000

    fix the contact form bug

--- CleanBlog.Web/scripts/contactForm.js
+++ CleanBlog.Web/scripts/contactForm.js
@@ -1,20 +1,19 @@
-﻿var contactFor = contactForm ||
+﻿var contactForm = contactForm ||
 {
     init: function () {
         this.listeners();
     },
-    listeners: function()
-    {
-        $(document).on("click", ".contact-submit", function (e)
-        {
-            e.preventDefault();
-            var form = $("contact-form");
-            form.submit();
-        }); 
+    listeners: function () {
+        $(document).on('click',
+            '.contact-submit',
+            function (e) {
+                e.preventDefault();
+                var form = $("#contactForm");
+                form.submit();
+            });
     },
     //OnSuccess = "contactForm.showResult", OnFailure = "contactForm.showResult"
-    showResult: function()
-    {
+    showResult: function () {
         $("#form-outer").hide("slow");
         $("#form-result").show("slow");
     }
